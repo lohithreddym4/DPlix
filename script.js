961f3c7dae97c8141bb8f97d9b278d9abeea4e8f
Venkata Lohith Kumar Reddy Mukkamalla <lohithreddym4@gmail.com>
2023-07-28 21:36:48 +0530

    implemented half

--- script.js
+++ script.js
@@ -1,8 +1,72 @@
 const div =document.getElementById("main")
 const genre=document.getElementById("genre-in")
-
-
+const white=document.getElementsByClassName("white")
 let times=0;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Nzc4MWY3MmJlZmVhMjdhNmQ2Y2E0ZDU3YzllMDAzYSIsInN1YiI6IjY0YzIyY2FlMmYxYmUwMDBhZTRjMTgzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Um-TRmWEBbwCKouhbnNKZWf5bdLPWG43dM5B5njcxA'
+  }
+};
+white[1].onclick=()=>{
+  div.innerHTML=' '
+  let movieEle=document.querySelector('.searchIN')
+  movieEle.innerHTML=''
+
+  let i=1;
+  while(i<=5){
+  
+    url=`https://api.themoviedb.org/3/trending/tv/week?api_key=47781f72befea27a6d6ca4d57c9e003a&language=en-US&page=${i}&page=${i}`
+    fetch(url, options)
+      .then(response => response.json())
+      .then(movies => {
+    for(let i=0;i<movies.results.length;i++)
+    {
+      let movie=movies.results[i]
+      var  imgElem=document.createElement('img')
+      imgElem.style.margin="5px"
+      imgElem.setAttribute('data-id', movie.id)
+      if(movie["poster_path"]==undefined)
+      {
+        continue;
+      }
+      imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+      movieEle.appendChild(imgElem)
+    }
+  })
+  i++
+}
+}
+white[0].onclick=()=>{
+  
+  div.innerHTML=' '
+  let movieEle=document.querySelector('.searchIN')
+  movieEle.innerHTML=''
+  let i=1;
+  while(i<=10){
+      url=`https://api.themoviedb.org/3/movie/top_rated?api_key=47781f72befea27a6d6ca4d57c9e003a&language=en-US&page=${i}`
+    fetch(url, options)
+      .then(response => response.json())
+      .then(movies => {
+    for(let i=0;i<movies.results.length;i++)
+    {
+      let movie=movies.results[i]
+      var  imgElem=document.createElement('img')
+      imgElem.style.margin="5px"
+      imgElem.setAttribute('data-id', movie.id)
+      if(movie["poster_path"]==undefined)
+      {
+        continue;
+      }
+      imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+      movieEle.appendChild(imgElem)
+  }
+  })
+  i++;
+}
+}
 function vis(){
   times+=1
   if(times%2==0)
@@ -10,13 +74,11 @@ function vis(){
   else
   genre.style.display="none"
 }
-
 window.onload = () => {
   originals()
   trending()
   topRated()
 }
-
 fetchMovies=(url,element,path_type)=>
 {
   fetch(url)
@@ -51,8 +113,6 @@ const originals=()=>{
   url='https://api.themoviedb.org/3/discover/movie?api_key=47781f72befea27a6d6ca4d57c9e003a&language=en-US'
   fetchMovies(url,'#originals','poster_path')
 }
-originals()
-
 const trending=()=>{
   url='https://api.themoviedb.org/3/trending/tv/week?api_key=47781f72befea27a6d6ca4d57c9e003a'
   fetchMovies(url,'#trending','backdrop_path')
@@ -61,37 +121,41 @@ const topRated=()=>{
   url='https://api.themoviedb.org/3/movie/top_rated?api_key=47781f72befea27a6d6ca4d57c9e003a&language=en-US&page=1'
   fetchMovies(url,'#top_rated','poster_path')
 }
+genre.onclick = e => {
+  div.innerHTML=''
+  gen(e.target.innerText)
+} 
+const gen=(genr)=>{
+  div.innerHTML=''
+  url=`https://api.themoviedb.org/3/discover/movie?with_genres=${genr}`
 
-
-
-const getTrailer=(id)=>{
-  var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=47781f72befea27a6d6ca4d57c9e003a&language=en-US`
-  fetch(url).then(response=>{
-    response.json()
-  }).then(json=>{
-    console.log(json)
+    div.innerHTML=''
+    fetch(url, options)
+      .then(response => response.json())
+      .then(movies => {
+        let movieEle=document.querySelector('.searchIN')
+        movieEle.innerHTML=''
+    for(let i=0;i<movies.results.length;i++)
+    {
+      let movie=movies.results[i]
+      var  imgElem=document.createElement('img')
+      imgElem.style.margin="5px"
+      imgElem.setAttribute('data-id', movie.id)
+      if(movie["poster_path"]==undefined)
+      {
+        continue;
+      }
+      imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+      movieEle.appendChild(imgElem)
+    }
   })
 }
-
-
-
-
 // ************************************
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Nzc4MWY3MmJlZmVhMjdhNmQ2Y2E0ZDU3YzllMDAzYSIsInN1YiI6IjY0YzIyY2FlMmYxYmUwMDBhZTRjMTgzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Um-TRmWEBbwCKouhbnNKZWf5bdLPWG43dM5B5njcxA'
-  }
-};
-
 const getSearch=()=>{
   div.innerHTML=''
   const key=document.getElementById('search').value
-  getDet(key)
-  
+  getDet(key)  
 }
-
 const getDet=(key)=>{
   document.getElementById('search').value=key
   fetch(`https://api.themoviedb.org/3/search/multi?query=${key}`, options)
@@ -113,126 +177,54 @@ const getDet=(key)=>{
     movieEle.appendChild(imgElem)
   }
 })
-    .catch(err => console.error(err));
-  
+.catch(err => console.error(err));
+}
+const funGen=(lang)=>
+{
+  div.innerHTML=''
+  let i=1
+  let movieEle=document.querySelector('.searchIN')
+  movieEle.innerHTML=''
+      while(i<10){
+      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=${lang}&page=${i}`, options)
+        .then(response => response.json())
+        .then(movies => {
+      for(let i=0;i<movies.results.length;i++)
+      {
+        let movie=movies.results[i]
+        var  imgElem=document.createElement('img')
+        imgElem.style.margin="5px"
+        imgElem.setAttribute('data-id', movie.id)
+        if(movie["poster_path"]==undefined)
+        {
+          continue;
+        }
+        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+        movieEle.appendChild(imgElem)
+      }
+    })
+    i++;
   }
+}
 
-    document.getElementById('tel').addEventListener('click',function()
-    {
-      div.innerHTML=''
-      
-      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=te`, options)
-        .then(response => response.json())
-        .then(movies => {
-          let movieEle=document.querySelector('.searchIN')
-          movieEle.innerHTML=''
-      for(let i=0;i<movies.results.length;i++)
-      {
-        let movie=movies.results[i]
-        var  imgElem=document.createElement('img')
-        imgElem.style.margin="5px"
-        imgElem.setAttribute('data-id', movie.id)
-        if(movie["poster_path"]==undefined)
-        {
-          continue;
-        }
-        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
-        movieEle.appendChild(imgElem)
-      }
-    })
+document.getElementById('tel').addEventListener('click',function()
+{
+  funGen('te')
 })
-    document.getElementById('hin').addEventListener('click',function()
-    {
-      div.innerHTML=''
-      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=hi`, options)
-        .then(response => response.json())
-        .then(movies => {
-          let movieEle=document.querySelector('.searchIN')
-          movieEle.innerHTML=''
-      for(let i=0;i<movies.results.length;i++)
-      {
-        let movie=movies.results[i]
-        var  imgElem=document.createElement('img')
-        imgElem.style.margin="5px"
-        imgElem.setAttribute('data-id', movie.id)
-        if(movie["poster_path"]==undefined)
-        {
-          continue;
-        }
-        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
-        movieEle.appendChild(imgElem)
-      }
-    })
+document.getElementById('hin').addEventListener('click',function()
+{
+  funGen('hi')
 })
-    document.getElementById('mal').addEventListener('click',function()
-    {
-      div.innerHTML=''
-      
-      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=ml`, options)
-        .then(response => response.json())
-        .then(movies => {
-          let movieEle=document.querySelector('.searchIN')
-          movieEle.innerHTML=''
-      for(let i=0;i<movies.results.length;i++)
-      {
-        let movie=movies.results[i]
-        var  imgElem=document.createElement('img')
-        imgElem.style.margin="5px"
-        imgElem.setAttribute('data-id', movie.id)
-        if(movie["poster_path"]==undefined)
-        {
-          continue;
-        }
-        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
-        movieEle.appendChild(imgElem)
-      }
-    })
+document.getElementById('tam').addEventListener('click',function()
+{
+  funGen('ta')
 })
-    document.getElementById('tam').addEventListener('click',function()
-    {
-      div.innerHTML=''
-      
-      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=ta`, options)
-        .then(response => response.json())
-        .then(movies => {
-          let movieEle=document.querySelector('.searchIN')
-          movieEle.innerHTML=''
-      for(let i=0;i<movies.results.length;i++)
-      {
-        let movie=movies.results[i]
-        var  imgElem=document.createElement('img')
-        imgElem.style.margin="5px"
-        imgElem.setAttribute('data-id', movie.id)
-        if(movie["poster_path"]==undefined)
-        {
-          continue;
-        }
-        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
-        movieEle.appendChild(imgElem)
-      }
-    })
+document.getElementById('mal').addEventListener('click',function()
+{
+  funGen('ml')
 })
-    document.getElementById('eng').addEventListener('click',function()
-    {
-      div.innerHTML=''
-      
-      fetch(`https://api.themoviedb.org/3/discover/movie?with_original_language=en`, options)
-        .then(response => response.json())
-        .then(movies => {
-          let movieEle=document.querySelector('.searchIN')
-          movieEle.innerHTML=''
-      for(let i=0;i<movies.results.length;i++)
-      {
-        let movie=movies.results[i]
-        var  imgElem=document.createElement('img')
-        imgElem.style.margin="5px"
-        imgElem.setAttribute('data-id', movie.id)
-        if(movie["poster_path"]==undefined)
-        {
-          continue;
-        }
-        imgElem.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
-        movieEle.appendChild(imgElem)
-      }
-    })
+document.getElementById('eng').addEventListener('click',function()
+{
+  funGen('en')
 })
+    
